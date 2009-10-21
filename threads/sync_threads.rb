1d91a85914fb88fdde9fc796fee09029e9d6547a
Sergey Kruk <sergey.kruk@gmail.com>
2009-10-21 23:58:37 +0400

    added mutex solution

--- threads/sync_threads.rb
+++ threads/sync_threads.rb
@@ -1,14 +1,35 @@
 $cnt = 0
 
-t3 = Thread.new do
+t1 = Thread.new do
   100000.times { $cnt += 1 }
 end
 
-t4 = Thread.new do
+t2 = Thread.new do
   100000.times { $cnt += 1 }
+end
+
+t1.join
+t2.join
+
+puts "Without sync: #{$cnt}"
+
+require "thread"
+$cnt = 0
+$mutex = Mutex.new
+
+t3 = Thread.new do
+  $mutex.synchronize do
+    100000.times { $cnt += 1 }
+  end
+end
+
+t4 = Thread.new do
+  $mutex.synchronize do
+    100000.times { $cnt += 1 }
+  end
 end
 
 t3.join
 t4.join
 
-puts $cnt
+puts "With sync: #{$cnt}"
