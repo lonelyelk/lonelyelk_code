a3635140879f7c3ede43dcf6095598d9aa0c1168
Sergey Kruk <sergey.kruk@gmail.com>
2011-02-04 23:26:51 +0300

    more different brackets

--- recursive_regexp/brackets.rb
+++ recursive_regexp/brackets.rb
@@ -1,9 +1,12 @@
-str = "1 + 2 * (3 - 4 / (5 + 6) + (7 - 8 * (9 + 10 * 11) + 12) * 13 - 14) + 15 * (16 - 17)"
+str = "1 + 2 * (3 - 4 / {5 + 6} + [7 - 8 * (9 + 10 * 11) + 12 * {13 - 14}] + 15) + 16 * (17 + 18)"
 
 re = %r{
         (?<fill>[0-9+\-*/\s]+){0}
-        (?<expression>\g<fill>*\g<bracket>\g<fill>*|\g<fill>){0}
-        (?<bracket>\(\g<expression>+\))
+        (?<expression>\g<fill>*\g<brackets>\g<fill>*|\g<fill>){0}
+        (?<braces>\{\g<expression>+\}){0}
+        (?<squarebrackets>\[\g<expression>+\]){0}
+        (?<parentheses>\(\g<expression>+\)){0}
+        (?<brackets>\g<braces>|\g<squarebrackets>|\g<parentheses>)
 }x
 
 def calculator(str)
@@ -18,10 +21,10 @@ f =-> s do
   if $~[:expression] == $~[:fill]
     calculator($~[:fill])
   else
-    calculator($~[:bracket][1..-2].gsub(re, &f))
+    calculator($~[:brackets][1..-2].gsub(re, &f))
   end
 end
 
 puts calculator(str.gsub(re, &f))
-puts eval(str)
+puts eval(str.gsub(/(?<left>\{|\[)|\}|\]/) { |s| $~[:left] ? "(" : ")" })
 
